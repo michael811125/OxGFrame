e6f3b72f8969843bc5b2bf1d20d4e78b04674fdd
MichaelO <michael811125@gmail.com>
2023-07-23 21:04:07 +0800

    modified

--- Assets/OxGFrame/MediaFrame/Scripts/Editor/AudioBaseEditor.cs
+++ Assets/OxGFrame/MediaFrame/Scripts/Editor/AudioBaseEditor.cs
@@ -7,26 +7,28 @@ namespace OxGFrame.MediaFrame.Editor
     [CustomEditor(typeof(AudioBase))]
     public class AudioBaseEditor : UnityEditor.Editor
     {
-        protected SerializedProperty audioLength;
+        protected AudioBase _target = null;
 
-        private AudioBase _target = null;
+        // Properties
+        protected SerializedProperty _audioLength;
 
         private void OnEnable()
         {
-            this.audioLength = serializedObject.FindProperty("audioLength");
+            this._target = (AudioBase)target;
+
+            // Init Properties
+            this._audioLength = serializedObject.FindProperty("audioLength");
         }
 
         public override void OnInspectorGUI()
         {
-            // 查找目標
-            this._target = (AudioBase)target;
-
             base.OnInspectorGUI();
 
-            this.ShowAudioLength();
+            // Draw Views
+            this.DrawAudioLengthView();
         }
 
-        protected virtual async void ShowAudioLength()
+        protected virtual async void DrawAudioLengthView()
         {
             var style = new GUIStyle(GUI.skin.box);
             style.normal.background = _Make2DTexture(1, 1, new Color(0f, 0.5f, 0.25f, 0.5f));
@@ -35,7 +37,7 @@ namespace OxGFrame.MediaFrame.Editor
 
             serializedObject.Update();
 
-            EditorGUILayout.PropertyField(this.audioLength);
+            EditorGUILayout.PropertyField(this._audioLength);
             if (GUILayout.Button("Preload"))
             {
                 // must set dirty (save will be success)
@@ -46,18 +48,18 @@ namespace OxGFrame.MediaFrame.Editor
                 {
                     case SourceType.Audio:
                         audioClip = this._target.audioClip;
-                        if (audioClip != null) this.audioLength.floatValue = this._target.audioLength = this._target.audioClip.length;
+                        if (audioClip != null) this._audioLength.floatValue = this._target.audioLength = this._target.audioClip.length;
                         else Debug.LogError("Cannot found AudioClip");
                         break;
 
                     case SourceType.Streaming:
                         audioClip = await this._target.GetAudioFromStreamingAssets(false);
-                        if (audioClip != null) this.audioLength.floatValue = this._target.audioLength = audioClip.length;
+                        if (audioClip != null) this._audioLength.floatValue = this._target.audioLength = audioClip.length;
                         break;
 
                     case SourceType.Url:
                         audioClip = await this._target.GetAudioFromURL(false);
-                        if (audioClip != null) this.audioLength.floatValue = this._target.audioLength = audioClip.length;
+                        if (audioClip != null) this._audioLength.floatValue = this._target.audioLength = audioClip.length;
                         break;
                 }
 
